f7147113fc5cc7c0aedf11a22946f7d10a9c1985
Miguel Julio <fitfab@yahoo.com>
2021-01-23 14:10:24 -0500

    wip in progress

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
+
 module.exports = {
-    plugins: ['babel-plugin-styled-components'],
-    presets: ['@babel/preset-env', '@babel/preset-react']
-  };
+  presets: ["@babel/env", "@babel/typescript", "@babel/react"],
+  plugins: [
+      [
+          "babel-plugin-styled-components",
+          {
+              // Better Debugging -- adds className and displayName
+              "displayName": true,
+              // Suport for SSR
+              "ssr": true,
+              // Template String Transpilation -- smaller output than "babel-preset-latest"
+              "transpileTemplateLiterals": true,
+              // Dead Code Elimination
+              "pure": true
+          }
+      ],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      // "@babel/proposal-class-properties",
+      "@babel/proposal-object-rest-spread"
+  ]
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,27 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel'
 import pkg from './package.json';
+import path from 'path'
 
 import analyze from 'rollup-plugin-analyzer'
 
 import external from 'rollup-plugin-peer-deps-external'
 
 const extensions = ['.ts', '.tsx']
+const PACKAGE_ROOT =  './packages/**/*/index.js'
+
+const wd = path.resolve(__dirname, 'packages/button/src/index.js' )
+console.log('wd: ', wd);
 
 export default {
-    input: './src/index.ts',
+    input: wd, //'./src/index.ts',
     plugins: [
         external(),
         // Allows node_modules resolution
         resolve({ extensions }),
 
         // Compile TypeScript/JavaScript files
-        babel({ extensions, include: ['src/**/*'] }),
+        babel({ extensions, include: ['src/**/*'], babelHelpers: 'bundled' }),
         analyze({ limit: 0 }),
     ],
 
